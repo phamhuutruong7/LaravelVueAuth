aee6b6769b9992e78994fc2367498a5ebd164e79
Pham Huu Truong <pham.huutruong@appsfactory.de>
2023-08-06 19:06:56 +0200

    Initial commit

---
+++ VueBreezeApi/src/components/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from "../stores/auth"
+
+const authStore = useAuthStore();
+
+onMounted(async () => {
+    await authStore.getUser();
+});
+
+</script>
+
+<template>
+    <div>
+        <div v-if="authStore.user">
+            <h1>{{ authStore.user.name }}</h1>
+            <p>{{ authStore.user.email }}</p>
+        </div>
+        <div v-else>
+            <h1>Go and login</h1>
+        </div>
+    </div>
+</template>
