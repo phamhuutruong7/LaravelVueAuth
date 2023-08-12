e9e77aee7842faedf3fcd8715f5e60262440274b
Pham Huu Truong <pham.huutruong@appsfactory.de>
2023-08-12 15:52:29 +0200

    Update some change to env and frontend. Keep this project as repo.

--- VueBreezeApi/src/components/Nav.vue
+++ VueBreezeApi/src/components/Nav.vue
@@ -6,7 +6,7 @@ const authStore = useAuthStore()
 
 </script>
 <template>
-    <nav class="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+    <nav class="bg-indigo-900 text-white px-8 py-2.5 sm:px-4">
         <div
           class="container mx-auto flex flex-wrap items-center justify-between"
           bis_skin_checked="1"
